00a08a7f4c55f9e6034e7da4343aa6e0908b1eee
Luis Escobar <lae311@stern.nyu.edu>
2021-09-29 15:04:28 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    const gifts = [];
+    for (let i = 0; i < name.length; i++) {
+        gifts.push( `Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return gifts;
+}
+
+function countDown (x) {
+    while (x>=0) {
+        console.log (x--)
+    }
+}
